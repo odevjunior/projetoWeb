d3f7fe02e17edbd9663cd20cbd94a6724a196678
Studies <Jairo Junior>
2021-02-19 11:36:09 -0300

    adicionando classe estado ao projeto

---
+++ src/main/java/br/com/ProjetoWeb/domain/State.java
@@ -0,0 +1,22 @@
+package br.com.ProjetoWeb.domain;
+
+public class State {
+    private String name;
+    private String acronym;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+}
